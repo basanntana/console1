55f826c302e7ebc7d73d1dbdd12b72c3adfd2579
Bárbara Santana <44272676+basanntana@users.noreply.github.com>
2020-12-22 20:34:42 -0300

    Add files via upload

---
+++ teste.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, x;
+            int I;
+            I = 1;
+
+            while (I <= 4)
+            {
+                Console.Write("Digite um número:");
+                a = double.Parse(Console.ReadLine());
+                x = a * 3;
+                Console.WriteLine("" + x);
+                I++;
+
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
